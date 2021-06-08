a33fe1752f3e6b6a5ba34672acf5b4e1e4f4529f
Renan <souza.rnn@gmail.com>
2021-06-07 22:53:55 -0300

    Backend project created

---
+++ backend/src/main/java/com/rennansouza/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,13 @@
+package com.rennansouza.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rennansouza/dslearnbds/DslearnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rennansouza.dslearnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
