d1c4cf8b8a02f72837643e92fc1a9d8f38186713
Renan <souza.rnn@gmail.com>
2021-06-10 22:01:23 -0300

    Custom service level authorization

---
+++ backend/src/main/java/com/rennansouza/dslearnbds/services/exceptions/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package com.rennansouza.dslearnbds.services.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public UnauthorizedException(String msg) {
+		super(msg);
+	}
+	
+	
+}
