625de49c84d7562e0767ff08016456064a325956
Renan <souza.rnn@gmail.com>
2021-06-10 22:47:11 -0300

    Custom content for current user

--- backend/src/main/java/com/rennansouza/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/com/rennansouza/dslearnbds/repositories/NotificationRepository.java
@@ -1,9 +1,13 @@
 package com.rennansouza.dslearnbds.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rennansouza.dslearnbds.entities.Notification;
+import com.rennansouza.dslearnbds.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+	Page<Notification> findByUser(User user, Pageable pageable);
 }
